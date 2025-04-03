69e7153305d53001f01d84f87f718a95733f846d
Chechere <cesartjmv@gmail.com>
2025-04-03 11:48:06 +0200

    Fix: Arreglado tamaños y disposición pagina desarrollador

--- front/desarrollador/index.js
+++ front/desarrollador/index.js
@@ -1,3 +1,14 @@
+maxspeedchanged(document.getElementById("controlvelocidadmax").value);
+
 function maxspeedchanged(value) {
-	console.log("max speed: " + value);
+	let label = document.getElementById("velocidadmaxlabel");
+	label.innerText = "Máxima Velocidad Coche (Actual: " + value + "%):"
+}
+
+function nuevaPartida() {
+	console.log("TODO: Hacer nueva partida");
+}
+
+function iniciarCarrera() {
+	console.log("TODO: Iniciar nueva carrera");
 }
